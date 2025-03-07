30494d41348e5d5ed0a1b32489e3f66344d32f93
Wilhelm Lee <wilhelm_lee@dlsu.edu.ph>
2025-03-07 22:29:43 +0800

    revert changes to postSchema

--- models/postModel.js
+++ models/postModel.js
@@ -6,7 +6,7 @@ const postSchema = new mongoose.Schema({
     community: { type: mongoose.Schema.Types.ObjectId, ref: 'community', required: true },
     title: { type: String, required: true },
     content: { type: String, required: true },
-    upvotes: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    upvotes: { type: Number, default: 0 },
     downvotes: { type: Number, default: 0 },
     replies: { type: mongoose.Schema.Types.ObjectId, ref: 'reply' }
 });
